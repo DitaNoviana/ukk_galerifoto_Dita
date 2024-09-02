cd5ee84a167ffd0f5f5d2d3cd4ea7fe54208f8bb
DitaNoviana <novianadita275@gmail.com>
2024-09-02 08:50:30 +0800

    first commit

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	<div class="row">
 		<div class="col-md-3">
 			<div class="card">
-				<img src="kucing.jpg" class="card-img-top" title=""style="height: 12rem;">
+				<img src="" class="card-img-top" title=""style="height: 12rem;">
 				<div class="card-footer text-center">
 					<a href="">10 suka</a>
 					<a href="">3 komentar</a>
